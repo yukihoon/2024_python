fded96cd548368d39fc1bbd6aad4cfe99a11b258
yukihoon <166720665+yukihoon@users.noreply.github.com>
2024-04-11 21:54:32 +0900

    Add files via upload

---
+++ day2/day1.py
@@ -0,0 +1,20 @@
+#유저에게
+#첫 번째 정수 입력
+#두 번째 정수 입력
+#받은 후 두 수의 합을 출력하는 프로그램
+
+
+num1=input ("첫번째 정수 입력")
+num2=input("두번째 정수 입력")
+
+result = int(num1) + int(num2)
+print(f"결과: {result}")
+
+
+age=input(" 나이를 입력하세요")
+year=2024 -  int(age) + 1
+print(f"나이가 {age}살이라면 출생년도5는{year} 년 입니다")
+
+side =input("한변의 길이는")
+width =int(side)*int(side)
+print(f"한변의 {side}면 넓이는 {width}입니다")
